ace968ae6e0200b5c7a7ae0f6c1de1710b982158
Jordy Aparecido Faria de Araújo <jordyfaria@hotmail.com>
2018-06-21 13:53:49 -0300

    correções

--- Sintatic.py
+++ Sintatic.py
@@ -12,67 +12,61 @@ def panic(a,lx):
     return a
 
 
-# ------------------------   Determina quantos estados desempilhar  ----------------------------
-def pop(produtions,prod,pilha):
-    for i in range(len(produtions.loc[prod, 'produtions'].split())):
-        pilha.pop()  # Desempilha o tamanho da produção
+# ------------------------ Desempilha o tamanho da produção ----------------------------
+def pop(pilha,tamanho):
+    lista = []
+    for i in range(tamanho):
+        lista.append(pilha.pop())
+    return lista
 
-def popN(n,pilha):
-    for i in n:
-        x = pilha.pop()
-    return x
 
 # -----------------------------  Tradução dirigida  ------------------------------------
-def semantic(prod,file,ss,a,lx):  # Numero da produção, arquivo final, pilha semântica, ultimo token, objeto lexico
-
+# Numero da produção, arquivo final, pilha semântica, objeto lexico, token não terminal, lista da produção
+def semantic(prod,file,ss,lx,token,lenght):
+    lista = pop(ss,lenght+1)
     if prod is 5:
         file.write("\n\n\n")
     elif prod is 6:
-        lx.symbols_table[a[0]][1] = ss.pop()[1]  # id.tipo <- TIPO.tipo
-        ss.append(lx.symbols_table[a])
-        file.write(lx.symbols_table[a[0]][1]," ",lx.symbols_table[a[0]][2],";\n")  # Imprimir ( TIPO.tipo id.lexema ; )
+        lx.symbols_table[lista[-lenght-1][2]][1] = lista[-lenght][1]  # id.tipo <- TIPO.tipo
+        file.write(lista[-lenght][1]," ",lx.symbols_table[lista[-lenght-1][2]][2],";\n")  # Imprimir ( TIPO.tipo id.lexema ; )
     elif prod is 7:
-        TIPO = ['TIPO','','TIPO']
-        TIPO[1] = ss.pop()[1]       # TIPO.tipo <- int.tipo
-        ss.append(TIPO)
+        token[1] = lista[-lenght][1]  # TIPO.tipo <- int.tipo
+        ss.append(token)
     elif prod is 8:
-        TIPO = ['TIPO', '', 'TIPO']
-        TIPO[1] = ss.pop()[1]       # TIPO.tipo <- real.tipo
-        ss.append(TIPO)
+        token[1] = lista[-lenght][1]  # TIPO.tipo <- real.tipo
+        ss.append(token)
     elif prod is 9:
-        TIPO = ['TIPO', '', 'TIPO']
-        TIPO[1] = ss.pop()[1]       # TIPO.tipo <- lit.tipo
-        ss.append(TIPO)
+        token[1] = lista[-lenght][1]  # TIPO.tipo <- lit.tipo
+        ss.append(token)
     elif prod is 11:
-        if lx.symbols_table[a[0]][1] is not '':
-            if lx.symbols_table[a[0]][1] is "lit":
-                file.write("\tscanf(“%s”,",lx.symbols_table[a[0]][2],");\n")
-            elif lx.symbols_table[a[0]][1] is "int":
-                file.write("\tscanf(“%d”,", lx.symbols_table[a[0]][2], ");\n")
-            elif lx.symbols_table[a[0]][1] is "real":
-                file.write("\tscanf(“%lf”,", lx.symbols_table[a[0]][2], ");\n")
+        if ss[-1][1] is not '':
+            if ss[-1][1] is "lit":
+                file.write("\tscanf(“%s”,",ss[-1][2],");\n")
+            elif ss[-1][1] is "int":
+                file.write("\tscanf(“%d”,", ss[-1][2], ");\n")
+            elif ss[-1][1] is "real":
+                file.write("\tscanf(“%lf”,", ss[-1][2], ");\n")
         else:
             print("\nErro: Variável não declarada\n")
     elif prod is 12:
         file.write("\tprintf(",ss[-1][2],");\n")
     elif prod is 13:
-        ARG = ss.pop()      # ARG.atributos <- literal.atributos
-        ss.append(ARG)
+        token[1:] = ss.pop()[1:]      # ARG.atributos <- literal.atributos
+        ss.append(token)
     elif prod is 14:
-        ARG = ss.pop()      # ARG.atributos <- num.atributos
-        ss.append(ARG)
+        token[1:] = ss.pop()[1:]      # ARG.atributos <- num.atributos
+        ss.append(token)
     elif prod is 15:
-        if lx[a][1] is not '':
-            ARG = ss.pop()  # ARG.atributos <- id.atributos
-            ss.append(ARG)
+        if ss[-1][1] is not '':
+            token[1:] = ss.pop()[1:]  # ARG.atributos <- id.atributos
+            ss.append(token)
         else:
             print("\nErro: Variável não declarada\n")
     elif prod is 17:
-        if lx[a][1] is not '':
-            LD = ss.pop()
-            rcb = ss.pop()
-            if LD[1] is rcb[1]:
-                file.write(lx[a[1][2]," ",rcb[1]," ",LD[2]])  # id.lexema rcb.tipo LD.lexema
+        if ss[-1][1] is not '':
+            lista = pop(ss,lenght)
+            if lista[2][1] is lista[1][1]:
+                file.write(lista[0][1]," ",lista[1][1]," ",lista[2][1])  # id.lexema rcb.tipo LD.lexema
             else:
                 print("\nErro: Tipos diferentes para atribuição\n")
         else:
@@ -83,23 +77,20 @@ def semantic(prod,file,ss,a,lx):  # Numero da produção, arquivo final, pilha s
         LD = ss.pop()  # LD.atributos <- OPRD.atributos
         ss.append(LD)
     elif prod is 20:
-        if lx[a][1] is not '':
-            OPRD = ss.pop() # OPRD.atributos <- id.atributos
-            ss.append(OPRD)
+        if ss[-1][1] is not '':
+            token[1:] = ss.pop()[1:]  # OPRD.atributos <- id.atributos
+            ss.append(token)
         else:
             print("\nErro: Variável não declarada.\n")
     elif prod is 21:
-        OPRD = ss.pop()  # OPRD.atributos <- num.atributos
-        ss.append(OPRD)
+        token[1:] = ss.pop()[1:]  # OPRD.atributos <- num.atributos
+        ss.append(token)
     elif prod is 23:
         file.write("}\n")
     elif prod is 24:
-        EXP_R = ss.popN(3,ss)
-        file.write("if(",EXP_R[2],"){\n")
+        lista = pop(ss, lenght)   # CABEÇALHO <- se ( EXP_R ) entao
+        file.write("if(",lista[2][2],"){\n")
     # elif prod is 25:
-
-
-
 
 
 
@@ -145,14 +136,17 @@ def main():
             # --------------------- Reduce A -> B -----------------------------
             elif ACTION.loc[s, a[0]][0] is 'R':
                 prod = int(ACTION.loc[s, a[0]][1:])  # Recebe o numero da produção
-                pop(produtions,prod,stack)
+                lenght = len(produtions.loc[prod, 'produtions'].split())
+                pop(stack, lenght)
 
                 t = int(stack[-1])  # Faça o estado t ser o topo da pilha
                 stack.append(int(GOTO.loc[t,produtions.loc[prod,'non_terminal']]))  # Empilhe GOTO[t,A] na pilha
                 print(produtions.loc[int(prod),'non_terminal']+" -> "+produtions.loc[int(prod),'produtions'])  # Imprime a producao
 
                 ''' ---- Semantic ----- '''
-                semantic(prod,file,ss,a,lx)
+                token = [produtions.loc[int(prod),'non_terminal'],'','']  # token não terminal
+                print(token)
+                semantic(prod,file,ss,lx,token,lenght)
 
             # -------------------------- Accept -------------------------------
             elif ACTION.loc[s, a[0]] == 'accept':

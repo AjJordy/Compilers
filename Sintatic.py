efa0f5585a647007c52d1e861511cc25d6d89ab8
Jordy Aparecido Faria de Araújo <jordyfaria@hotmail.com>
2018-06-22 16:49:01 -0300

    some corrections

--- Sintatic.py
+++ Sintatic.py
@@ -22,79 +22,82 @@ def pop(pilha,tamanho):
 
 # -----------------------------  Tradução dirigida  ------------------------------------
 # Numero da produção, arquivo final, pilha semântica, objeto lexico, token não terminal, lista da produção
-def semantic(prod,file,ss,lx,token,lenght):
+def semantic(prod,file,ss,lx,token,lenght,T):
+
     resto = ss[-lenght:]
     ss = ss[:-lenght]
+    ss.append(token)
+
     if prod is 5:
         file.write("\n\n\n")
     elif prod is 6:
-        lx.symbols_table[ss[-1][2]][1] = ss[-lenght][1]  # id.tipo <- TIPO.tipo
-        file.write("\t"+ss[-lenght-1][1]+" "+ss[-lenght][2]+";\n")   # Imprimir ( TIPO.tipo id.lexema ; )
+        lx.symbols_table[resto[0][2]][1] = resto[1][1]  # id.tipo <- TIPO.tipo
+        file.write("\t"+resto[1][1]+" "+resto[0][2]+";\n")   # Imprimir ( TIPO.tipo id.lexema ; )
     elif prod is 7:
-        token[1] = ss[-lenght][1]  # TIPO.tipo <- int.tipo
-        ss.append(token)
+        ss[-1][1] = resto[-1][1]  # TIPO.tipo <- int.tipo
     elif prod is 8:
-        token[1] = ss[-lenght][1]  # TIPO.tipo <- real.tipo
-        ss.append(token)
+        ss[-1][1] = resto[-1][1]  # TIPO.tipo <- real.tipo
     elif prod is 9:
-        token[1] = ss[-lenght][1]  # TIPO.tipo <- lit.tipo
-        ss.append(token)
+        ss[-1][1] = resto[-1][1]  # TIPO.tipo <- lit.tipo
     elif prod is 11:
-        if ss[-1][1] is not '':
-            if ss[-1][1] is "lit":
-                file.write("\tscanf(“%s”,",ss[-1][2],");\n")
-            elif ss[-1][1] is "int":
-                file.write("\tscanf(“%d”,", ss[-1][2], ");\n")
-            elif ss[-1][1] is "real":
-                file.write("\tscanf(“%lf”,", ss[-1][2], ");\n")
+        if resto[-2][1] is not '':
+            if resto[-2][1] is "lit":
+                file.write("\tscanf(“%s”,&"+resto[-2][2]+");\n")
+            elif resto[-2][1] is "int":
+                file.write("\tscanf(“%d”,&"+ resto[-2][2]+ ");\n")
+            elif resto[-2][1] is "real":
+                file.write("\tscanf(“%lf”,&"+ resto[-2][2]+ ");\n")
         else:
             print("\nErro: Variável não declarada\n")
     elif prod is 12:
-        print(lenght)
-        print(str(ss))
-        file.write("\tprintf("+ss[-lenght][2]+");\n\t")
+        file.write("\tprintf("+resto[-2][1]+");\n")
     elif prod is 13:
-        token[1:] = ss[-1][1:]   # ARG.atributos <- literal.atributos
+        token[1:] = resto[-1][2:]   # ARG.atributos <- literal.atributos
         print("new "+str(token))
-        ss.append(token)
     elif prod is 14:
+        print(lenght)
+        print("\n\nResto " + str(resto))
         token[1:] = ss[-1][1:]      # ARG.atributos <- num.atributos
-        ss.append(token)
     elif prod is 15:
         if ss[-1][1] is not '':
             token[1:] = ss[-1][1:]  # ARG.atributos <- id.atributos
-            ss.append(token)
         else:
             print("\nErro: Variável não declarada\n")
     elif prod is 17:
-        if ss[-1][1] is not '':
-            if ss[-lenght][1] is ss[-lenght-1][1]:
-                file.write(lenght[-lenght][1]+" "+lenght[-lenght-1][1]+" "+lenght[-lenght-2][1])  # id.lexema rcb.tipo LD.lexema
+        if resto[-1][1] is not '':
+            if resto[-1][1] is resto[-2][1]:
+                file.write(resto[-1][1]+" "+resto[-2][1]+" "+resto[-3][1])  # id.lexema rcb.tipo LD.lexema
             else:
                 print("\nErro: Tipos diferentes para atribuição\n")
         else:
             print("\nErro: Variável não declarada\n")
-    # elif prod is 18:
+    #elif prod is 18:
+
 
     elif prod is 19:
-        LD = ss[-1]  # LD.atributos <- OPRD.atributos
-        ss.append(LD)
+        ss[-1][1:] = ss[-2][1:]  # LD.atributos <- OPRD.atributos
     elif prod is 20:
-        if ss[-1][1] is not '':
-            token[1:] = ss[-1][1:]  # OPRD.atributos <- id.atributos
-            ss.append(token)
+        if resto[-1][1] is not '':
+            token[1:] = resto[-1][1:]  # OPRD.atributos <- id.atributos
         else:
             print("\nErro: Variável não declarada.\n")
     elif prod is 21:
-        token[1:] = ss[-1][1:]  # OPRD.atributos <- num.atributos
-        ss.append(token)
+        token[1:] = ss[-2][1:]  # OPRD.atributos <- num.atributos
     elif prod is 23:
         file.write("}\n")
     elif prod is 24:
         # CABEÇALHO <- se ( EXP_R ) entao
-        file.write("\tif("+ss[-lenght][2]+"){\n\t")
-    # elif prod is 25:
-    return ss
+        file.write("\tif("+resto[-3][2]+"){\n\t")
+    elif prod is 25:
+        if resto[-3][1] == resto[-1][1]:
+            Tx = str(resto[-3][1])+str(resto[-2][2])+str(resto[-1][1])
+            T.append(Tx)
+            ss[-1][2] = Tx
+            file.write(Tx)
+        else:
+            print("\nErro: Operandos com tipos incompatíveis\n")
+
+    return ss,T
 
 
 # ------------------------ Inicializa o arquivo em linguagem C ---------------------------------
@@ -117,6 +120,7 @@ def main():
     # Variables
     stack = [0]  # States stack
     ss = []      # Semantic stack
+    T = []  # Variáveis temporárias
 
     # Final source
     file = initFile()
@@ -129,12 +133,12 @@ def main():
             #print(a)
             # --------------------- Shift t ------------------------------------
             if ACTION.loc[s, a[0]][0] is 'S':
+                ''' --- Semantic ---- '''
+                ss.append(a)
+
                 t = ACTION.loc[s, a[0]][1:]         # Recebe estado da ação
                 stack.append(int(t))                # Empilha t na pilha
                 a = lx.next_token()                 # Seja a o proximo simbulo da entrada
-
-                ''' --- Semantic ---- '''
-                ss.append(a)
 
             # --------------------- Reduce A -> B -----------------------------
             elif ACTION.loc[s, a[0]][0] is 'R':
@@ -148,9 +152,11 @@ def main():
 
                 ''' ---- Semantic ----- '''
                 token = [produtions.loc[int(prod),'non_terminal'],'','']  # token não terminal
-                print(token)
-                print(prod)
-                ss = semantic(prod,file,ss,lx,token,lenght)
+                #print(token)
+                #print(prod)
+                #print(lenght)
+
+                ss,T = semantic(prod,file,ss,lx,token,lenght,T)
 
             # -------------------------- Accept -------------------------------
             elif ACTION.loc[s, a[0]] == 'accept':

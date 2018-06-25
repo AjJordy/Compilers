c5349eee9dd017b91c08e6c6137b74e92e7a2384
Jordy Aparecido Faria de Araújo <jordyfaria@hotmail.com>
2018-06-25 15:13:39 -0300

    finish

--- Sintatic.py
+++ Sintatic.py
@@ -32,7 +32,12 @@ def semantic(prod,file,ss,lx,token,lenght,T):
         file.write("\n")
     elif prod is 6:
         lx.symbols_table[resto[0][2]][1] = resto[1][1]  # id.tipo <- TIPO.tipo
-        file.write("\t"+resto[1][1]+" "+resto[0][2]+";\n")   # Imprimir ( TIPO.tipo id.lexema ; )
+        if resto[1][1] is 'int':
+            file.write("\t"+resto[1][1]+" "+resto[0][2]+";\n")   # Imprimir ( TIPO.tipo id.lexema ; )
+        elif resto[1][1] is 'real':
+            file.write("\tdouble " + resto[0][2] + ";\n")
+        elif resto[1][1] is 'lit':
+            file.write("\tchar* " + resto[0][2] + ";\n")
     elif prod is 7:
         ss[-1][1] = resto[-1][1]  # TIPO.tipo <- int.tipo
     elif prod is 8:
@@ -44,30 +49,38 @@ def semantic(prod,file,ss,lx,token,lenght,T):
             if resto[-2][1] is "lit":
                 file.write("\tscanf(“%s”,&"+resto[-2][2]+");\n")
             elif resto[-2][1] is "int":
-                file.write("\tscanf(“%d”,&"+ resto[-2][2]+ ");\n")
+                file.write("\tscanf(“%d”,&"+ resto[-2][2]+");\n")
             elif resto[-2][1] is "real":
-                file.write("\tscanf(“%lf”,&"+ resto[-2][2]+ ");\n")
+                file.write("\tscanf(“%lf”,&"+ resto[-2][2]+");\n")
         else:
             print("\nErro: Variável não declarada\n")
     elif prod is 12:
-        file.write("\tprintf("+resto[-2][1]+");\n")
+        if resto[-2][1] is 'int':
+            file.write("\tprintf(\"%d\"," + resto[-2][2] + ");\n")
+        elif resto[-2][1] is 'real':
+            file.write("\tprintf(\"%lf\"," + resto[-2][2] + ");\n")
+        elif resto[-2][1] is 'lit':
+            file.write("\tprintf(\"%s\"," + resto[-2][2] + ");\n")
+        else:
+            file.write("\tprintf("+resto[-2][2]+");\n")
     elif prod is 13:
-        ss[-1][1:] = resto[-1][2:]   # ARG.atributos <- literal.atributos
-        #print("new "+str(token))
+        ss[-1][1:] = resto[-1][1:]   # ARG.atributos <- literal.atributos
     elif prod is 14:
-        print(lenght)
-        print("\n\nResto " + str(resto))
-        token[1:] = ss[-1][1:]      # ARG.atributos <- num.atributos
+        ss[-1][1:] = resto[-1][1:]   # ARG.atributos <- num.atributos
     elif prod is 15:
-        if ss[-1][1] is not '':
-            token[1:] = ss[-1][1:]  # ARG.atributos <- id.atributos
+        if resto[-1][1] is not '':
+            ss[-1][1:] = resto[-1][1:]  # ARG.atributos <- id.atributos
         else:
             print("\nErro: Variável não declarada\n")
     elif prod is 17:
-        if resto[-1][1] is not '':
-            if resto[-1][1] is resto[-2][1]:
-                file.write(resto[-1][1]+" "+resto[-2][1]+" "+resto[-3][1])  # id.lexema rcb.tipo LD.lexema
+        if resto[0][1] is not '':
+            if resto[0][1] is resto[2][1]:
+                if resto[2][2] in T:  # Se for variável temporária
+                    file.write("\t"+resto[0][2]+" = T"+str(T.index(resto[2][2]))+";\n")
+                else:  # Se não for variável temporária
+                    file.write("\t"+resto[0][2]+" = "+resto[2][2]+";\n")  # id.lexema rcb.tipo LD.lexema
             else:
+                print(str(resto[0][1])+" e "+str(resto[2][1]))
                 print("\nErro: Tipos diferentes para atribuição\n")
         else:
             print("\nErro: Variável não declarada\n")
@@ -75,13 +88,14 @@ def semantic(prod,file,ss,lx,token,lenght,T):
         if resto[-3][1] == resto[-1][1] and resto[-1][1] is not "lit":
             Tn = str(resto[-3][2])+str(resto[-2][2])+str(resto[-1][2])
             T.append(Tn)
+            ss[-1][1] = resto[-1][1]
             ss[-1][2] = Tn
-            file.write("\tT" + str(len(T)) + " = " + Tn + ";\n")
+            file.write("\tT" + str(len(T)-1) + " = " + Tn + ";\n")
         else:
             print("\nErro: Operandos com tipos incompatíveis\n")
 
     elif prod is 19:
-        ss[-1][1:] = ss[-2][1:]  # LD.atributos <- OPRD.atributos
+        ss[-1][1:] = resto[-1][1:]  # LD.atributos <- OPRD.atributos
     elif prod is 20:
         if resto[-1][1] is not '':
             ss[-1][1:] = resto[-1][1:]  # OPRD.atributos <- id.atributos
@@ -92,15 +106,17 @@ def semantic(prod,file,ss,lx,token,lenght,T):
     elif prod is 23:
         file.write("\t}\n")
     elif prod is 24:
-        file.write("\tif(T"+str(len(T))+"){\n\t")  # CABEÇALHO <- se ( EXP_R ) entao
+        file.write("\tif(T"+str(len(T)-1)+"){\n\t")  # CABEÇALHO <- se ( EXP_R ) entao
     elif prod is 25:
         if resto[-3][1] == resto[-1][1] and resto[-1][1] is not "lit":
             Tx = str(resto[-3][2])+str(resto[-2][2])+str(resto[-1][2])
             T.append(Tx)
             ss[-1][2] = Tx
-            file.write("\tT"+str(len(T))+" = "+Tx+";\n")
+            file.write("\tT"+str(len(T)-1)+" = "+Tx+";\n")
         else:
             print("\nErro: Operandos com tipos incompatíveis\n")
+    elif prod is 30:
+        file.write("}")
     return ss,T
 
 
@@ -114,7 +130,7 @@ def initFile(T):
         file.write("#include<stdio.h>\ntypedef char literal[256];"
                    "\n\nvoid main(void){\n\t/*----Variaveis temporarias----*/\n")
         for i in range(len(T)):
-            file.write("\tint T"+str(i+1)+";\n")
+            file.write("\tint T"+str(i)+";\n")
         file.write("\t/*---------------------------*/\n")
         file.writelines(data)
     return file
@@ -143,9 +159,9 @@ def main():
     #------------------Sintatic algorithm - Shift Reduce------------------------
     a = lx.next_token()
     while True:
-        #try:
+
+        try:
             s = stack[-1]                           # Seja s o estado no topo da pilha
-            #print(a)
             # --------------------- Shift t ------------------------------------
             if ACTION.loc[s, a[0]][0] is 'S':
                 ''' --- Semantic ---- '''
@@ -167,11 +183,9 @@ def main():
 
                 ''' ---- Semantic ----- '''
                 token = [produtions.loc[int(prod),'non_terminal'],'','']  # token não terminal
-                #print(token)
-                #print(prod)
-                #print(lenght)
 
                 ss,T = semantic(prod,file,ss,lx,token,lenght,T)
+
 
             # -------------------------- Accept -------------------------------
             elif ACTION.loc[s, a[0]] == 'accept':
@@ -179,14 +193,15 @@ def main():
                 initFile(T)
                 break
 
-        # except:
-        #     print("\nSintatic error at line " + str(a[3]) + " at column " + str(a[4]), "\n")
-        #     # a = panic(a,lx)
-        #     # if a is 'end':
-        #     #     break
-        #     # else:
-        #     #     continue
-        #     break
+        except:
+            print("\nSintatic error at line " + str(a[3]) + " at column " + str(a[4]), "\n")
+            if a[0] is 'erro': break
+            # a = panic(a,lx)
+            # if a is 'end':
+            #     break
+            # else:
+            #     continue
+            # break
 
 
 if __name__ == "__main__":
